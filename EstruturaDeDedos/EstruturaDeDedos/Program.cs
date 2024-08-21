32c8417313fe8c6eb0b21b9b06ef45f1cc0c8e68
Matheus2306 <Math2306.r@gmail.com>
2024-08-21 16:45:47 -0300

    termino da estrutura de dados (lista e pilha)

--- EstruturaDeDedos/EstruturaDeDedos/Program.cs
+++ EstruturaDeDedos/EstruturaDeDedos/Program.cs
@@ -67,6 +67,63 @@
             Console.WriteLine($"{carro.Key} - {carro.Value}");
         }
 
+        // trabalhando com fila 
+        /* criar uma fila (Queue) */
+        Queue<string> filaBanco = new Queue<string>();
+
+        // adicionar elementos em uma fila 
+        filaBanco.Enqueue("André");
+        filaBanco.Enqueue("João");
+        filaBanco.Enqueue("Maria");
+        filaBanco.Enqueue("José");
+
+        foreach (var pessoa in filaBanco)
+        {
+            Console.WriteLine(pessoa);
+        }
+
+        // retirar o primeiro elemento de uma fila /
+        filaBanco.Dequeue();
+
+        Console.WriteLine(); //pula linha em branco
+        Console.WriteLine("===========================");
+        Console.WriteLine();
+        foreach (var pessoa in filaBanco)
+        {
+            Console.WriteLine(pessoa);
+        }
+
+        // verificar se existe um elemento na fila /
+
+        bool encontrou = filaBanco.Contains("José");
+
+        if (encontrou == true)
+        {
+            Console.WriteLine("A pessoa está na fila");
+        }
+        else
+        {
+            Console.WriteLine("A pessoa NÃO está na fila");
+        }
+        Console.WriteLine(); //pula linha em branco
+        Console.WriteLine("===========================");
+        Console.WriteLine();
+        // trabalhando com pilha (stack)
+        // criando uma pilha /
+        Stack<string> livros = new Stack<string>();
+
+        // adicionar elementos em uma pilha */
+        livros.Push("Chapeuzinho Vermelho");
+        livros.Push("BRanca de Neve e os Sete Anões");
+        livros.Push("Princesa e o Sapo");
+
+        foreach (var livro in livros)
+        {
+            Console.WriteLine(livro);
+        }
+        Console.WriteLine(); //pula linha em branco
+        Console.WriteLine("===========================");
+        Console.WriteLine();
 
     }
 }
