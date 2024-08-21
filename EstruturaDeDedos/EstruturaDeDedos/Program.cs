3aacdc8c206fae3359f1cc3e7a8864610024c1a5
Matheus2306 <Math2306.r@gmail.com>
2024-08-21 14:54:06 -0300

    fnalizado os exemplos de list

--- EstruturaDeDedos/EstruturaDeDedos/Program.cs
+++ EstruturaDeDedos/EstruturaDeDedos/Program.cs
@@ -1,9 +1,58 @@
 ﻿internal class Program
 {
+
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("trabalhando com o Git\\Github");
-        Console.WriteLine("Mais uma tentativa de trabalhar com Git");
+
+
+        void listar(List<string> frutas)
+        {
+            Console.WriteLine("========================================");
+            Console.WriteLine("Minha lista de fruta");
+            foreach (var fruta in frutas)
+            {
+                Console.WriteLine(fruta);
+            }
+        }
+
+        //trabalhando com lista ( LIST )
+        List<string> frutas = new List<string>();
+
+
+        //adicionar itens  na lista
+        frutas.Add("Morango");
+        frutas.Add("Manga");
+        frutas.Add("Goiaba");
+        frutas.Add("Uva");
+        listar(frutas);
+        Console.WriteLine("========================================");
+        //outra forma de se imprimir na tela 
+        Console.WriteLine("comando mais simplificado");
+        frutas.ForEach(Console.WriteLine);
+
+
+        //inserir um elemento no índece especifico
+        frutas.Insert(1, "maracuja");
+        listar(frutas);
+
+
+        //alterar um elemento no índice específico
+        frutas[4] = "Melancia";
+        listar(frutas);
+
+        //remover elementos da lista no índice especifico
+        frutas.RemoveAt(3);
+
+        listar(frutas);
+        //removendo pelo valor do conteúdo
+        frutas.Remove("Morango");
+        listar(frutas);
+
+
+        //apagar todos os elementos 
+        frutas.Clear();
+        listar(frutas);
+
+
     }
 }
