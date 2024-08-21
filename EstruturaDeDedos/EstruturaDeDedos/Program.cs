dc401a79136657800d604e97b356fd8a97cf15d8
Matheus2306 <Math2306.r@gmail.com>
2024-08-21 10:38:38 -0300

    working with branchs, test 2

--- EstruturaDeDedos/EstruturaDeDedos/Program.cs
+++ EstruturaDeDedos/EstruturaDeDedos/Program.cs
@@ -3,5 +3,7 @@
     private static void Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
+        Console.WriteLine("trabalhando com o Git\\Github");
+        Console.WriteLine("Mais uma tentativa de trabalhar com Git");
     }
 }
